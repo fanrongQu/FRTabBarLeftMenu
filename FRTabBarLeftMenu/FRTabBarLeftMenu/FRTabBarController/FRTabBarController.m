cc09294c2ae016711e97226513df28c35bfb1814
sugeMarket <1366225686@qq.com>
2016-11-26 15:09:33 +0800

    FR
    修改FRTabBarController使支持个别页面横竖屏显示

--- FRTabBarLeftMenu/FRTabBarLeftMenu/FRTabBarController/FRTabBarController.m
+++ FRTabBarLeftMenu/FRTabBarLeftMenu/FRTabBarController/FRTabBarController.m
@@ -93,4 +93,20 @@
     }
 }
 
+
+- (BOOL)shouldAutorotate
+{
+    return [self.selectedViewController shouldAutorotate];
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return [self.selectedViewController supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [self.selectedViewController preferredInterfaceOrientationForPresentation];
+}
+
 @end
